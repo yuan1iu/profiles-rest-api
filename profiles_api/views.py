9fc7de56aab8b57569704860974912ebb704e63c
yuan <svooopy@gmail.com>
2022-04-26 03:16:49 +0800

    Added viewsets

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+
 from profiles_api import serializers
 
 
@@ -41,3 +43,39 @@ class HelloApiView(APIView):
         """Delete an object"""
 
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        api_view = [
+            'Use Actions',
+            'Autimatically maps URLs using Routers',
+            'Profide more functionality with less code'
+        ]
+        return Response({'message': 'Hello', 'api_view': api_view})
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        return Response({'http method': 'GET'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http method': 'PATCH'})
+
+    def update(self, request, pk=None):
+        return Response({'http method': 'PUT'})
+
+    def destroy(self, request, pk=None):
+        return Response({'http method': 'DELETE'})
